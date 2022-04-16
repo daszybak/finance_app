7be12229cfbf83d211fe624fe71ab82c7271fb7c
daszybak <karlo1diidd@gmail.com>
2022-04-16 16:44:44 +0200

    added routes and active class to navbar

--- src/containers/TopBar/TopBar.jsx
+++ src/containers/TopBar/TopBar.jsx
@@ -1,7 +1,13 @@
 import React from 'react';
 
 const TopBar = () => {
-  return <div className="p-3">Search</div>;
+  return (
+    <div className="fixed-top  d-flex justify-content-between bg-light p-4 ps-4">
+      <h5>Lobel Investing</h5>
+      <h6>Search</h6>
+      <p>Light/Dark Mode</p>
+    </div>
+  );
 };
 
 export default TopBar;
